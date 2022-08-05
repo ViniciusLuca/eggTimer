e85f3a5fdeb976332c0b0bef91cae1bf93bf2931
Viniciusluca <vini.luca17@gmail.com>
2022-08-05 15:30:38 -0400

    modified architecture

--- eggTimer.deluca/ViewController.swift
+++ eggTimer.deluca/ViewController.swift
@@ -8,67 +8,76 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var titleLabel: UILabel!
-    
+class Presenter{
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
-    
     let eggTimes = ["Soft": 300.0, "Medium": 420.0, "Hard": 720.0]
     
     var totalTime = 0.0
     var secondsPassed = 0.0
+    var player: AVAudioPlayer!
+    
+    func typeEgg(hardness: String) {
+        secondsPassed = 0.0
+        totalTime = eggTimes[hardness]!
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+    }
+    func checkIfTimeFinished() -> Bool {
+        return secondsPassed <= totalTime
+    }
+    func getProgress() -> Double {
+        return secondsPassed / totalTime
+    }
+    func countSeconds() {
+        secondsPassed += 1
+    }
+        
+}
+                    
+class ViewController: UIViewController {
+    
+    var presenter = Presenter()
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     
     var timer =  Timer()
-    
-    var player: AVAudioPlayer!
     
     @IBAction func typeEgg(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         print(hardness)
         
         progressBar.progress = 0.0
-        secondsPassed = 0.0
+        
         titleLabel.text = hardness
         
         timer.invalidate()
+        presenter.typeEgg(hardness: hardness)
         
-        totalTime = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
-        if hardness == "Hard"{
-            print("minutes: \(hardTime)")
-        }
-        else if hardness == "Medium"{
-            print("minutes: \(mediumTime)")
-        }
-        else if hardness == "Soft"{
-            print("minutes: \(softTime)")
-        }
-            
+       
     }
         @objc func updateTimer() {
-        if  secondsPassed <= totalTime {
-            
-            let percentProgress = secondsPassed / totalTime
-            progressBar.progress = Float(percentProgress)
-            secondsPassed += 1
+            if  presenter.checkIfTimeFinished() {
+            progressBar.progress = Float(presenter.getProgress())
+            presenter.countSeconds()
         }
         else {
             timer.invalidate()
             titleLabel.text = "DONE!"
-            playSound()
+            presenter.playSound()
         }
         
-        func playSound() {
-            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
-            player = try! AVAudioPlayer(contentsOf: url!)
-            player.play()
-    }
+        
 }
 
 }
